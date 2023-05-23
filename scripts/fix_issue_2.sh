88142028277c3b11799793d936fb6936fe86c343
Douglas Hawthorne <dfhawthorne@computer.org>
2023-05-23 11:28:27 +1000

    Partial fix to issue #2 * Moved files out of the `_/rsrc/` directory tree if a corresponding   directory exists elsewhere. * Updated HTML files to reflect new location

--- scripts/fix_issue_2.sh
+++ scripts/fix_issue_2.sh
@@ -11,8 +11,17 @@ do
         -e 's!https://sites.google.com/site/yetanotherocmoriginal/!https://dfhawthorne.github.io/!g' \
         -e 's!"/site/yetanotherocmoriginal/!"/!g' \
         -e 's!"/site/yetanotherocmoriginal"!"/"!g' \
+        -e 's!_/rsrc/[0-9][0-9]*/!!g' \
         "${file_name}"
-done < <(find . -type f)
+done < <(find . -type f -name "*.html")
+
+while read file_name
+do 
+    new_file_name=$(echo "${file_name}" | sed -e 's!_/rsrc/[0-9][0-9]*/!!g')
+    dir_name=$(dirname "${new_file_name}")
+    [[ -d "${dir_name}" && ! -f "${new_file_name}" ]] && \
+        git mv "${file_name}" "${new_file_name}"
+done < <(find _ -type f)
 
 popd 
 
