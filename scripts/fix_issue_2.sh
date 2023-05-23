d02d46a2f3c94e61aaebb32bdb21224e7cb155fd
Douglas Hawthorne <dfhawthorne@computer.org>
2023-05-23 23:44:49 +1000

    Partial issue of issue #2 * removed empty _/rsrc directories * removed duplicate resource files while keeping latest version * created .gitignore for test files and directories

--- scripts/fix_issue_2.sh
+++ scripts/fix_issue_2.sh
@@ -5,6 +5,10 @@
 
 pushd ~/Documents/dfhawthorne.github.io/docs
 
+# ------------------------------------------------------------------------------
+# Update HTML files to point to away from _/rsrc
+# ------------------------------------------------------------------------------
+
 while read file_name
 do
     sed -i \
@@ -15,13 +19,33 @@ do
         "${file_name}"
 done < <(find . -type f -name "*.html")
 
+# ------------------------------------------------------------------------------
+# Move files from _/rsrc
+# ------------------------------------------------------------------------------
+
 while read file_name
 do 
     new_file_name=$(echo "${file_name}" | sed -e 's!_/rsrc/[0-9][0-9]*/!!g')
     dir_name=$(dirname "${new_file_name}")
-    [[ -d "${dir_name}" && ! -f "${new_file_name}" ]] && \
-        git mv "${file_name}" "${new_file_name}"
+    if [[ -d "${dir_name}" ]]
+    then
+        [[ ! -f "${new_file_name}" || "${file_name}" -nt "${new_file_name}" ]] && \ 
+            git mv -f "${file_name}" "${new_file_name}"
+        [[ -f "${new_file_name}" ]] && \
+            git rm -f "${file_name}"
+    fi
 done < <(find _ -type f)
+
+# ------------------------------------------------------------------------------
+# Remove empty directories from _/rsrc
+# ------------------------------------------------------------------------------
+
+for dir in _/rsrc/*
+do
+    count=$(find "${dir}" -type f | wc -l)
+    [[ ${count} -eq 0 ]] && \
+        git rm -f -r "${dir}"
+done
 
 popd 
 
